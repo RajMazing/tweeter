d33e4867f3b8afd48c20e4b9910fe977cb865735
RajMazing <caseyraj92@gmail.com>
2021-12-16 07:31:55 +0000

    updated/refactored index.html by removing the articles and add jquery to the client.js to make tweet containers, updated all css files, made a counter with composer-char-counter.js

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,31 @@
 $(document).ready(function() {
-  // --- our code goes here ---
-});
- 
- 
- 
- const tweets = $.ajax({
-    url: '/tweets'
-  });
-  console.log(tweets);
+  
+  $("#tweet-text").on('keyup', function() {
+    let counter = $('.counter');
+    counter.text(140 - $(this).val().length);
+
+    if (counter.text() < 0) {
+      
+      counter.css("color", "red");
+      
+      console.log($('.counter'))
+    } else {
+      counter.css("color", "black");
+      
+    }
+  })
+  
+})
+// $("input").blur(function(){
+//   alert("This input field has lost its focus.");
+// });
+
+
+
+
+
+
+
+
+
+
