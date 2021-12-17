7c369959b5328fba224a40b3ad3a6b8db6391db2
RajMazing <caseyraj92@gmail.com>
2021-12-17 01:40:59 +0000

    added functionality to the submit button via client.js - now tweeter can update tweets without reloading the page

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,28 +7,28 @@
 
 
 const data = [
-  {
-    user: {
-      name: "Newton",
-      avatars: "https://i.imgur.com/73hZDYK.png",
-      handle: "@SirIsaac",
-    },
-    content: {
-      text: "If I have seen further it is by standing on the shoulders of giants",
-    },
-    created_at: 1461116232227,
-  },
-  {
-    user: {
-      name: "Descartes",
-      avatars: "https://i.imgur.com/nlhLi3I.png",
-      handle: "@rd",
-    },
-    content: {
-      text: "Je pense , donc je suis",
-    },
-    created_at: 1461113959088,
-  },
+  // {
+  //   user: {
+  //     name: "Newton",
+  //     avatars: "https://i.imgur.com/73hZDYK.png",
+  //     handle: "@SirIsaac",
+  //   },
+  //   content: {
+  //     text: "If I have seen further it is by standing on the shoulders of giants",
+  //   },
+  //   created_at: 1461116232227,
+  // },
+  // {
+  //   user: {
+  //     name: "Descartes",
+  //     avatars: "https://i.imgur.com/nlhLi3I.png",
+  //     handle: "@rd",
+  //   },
+  //   content: {
+  //     text: "Je pense , donc je suis",
+  //   },
+  //   created_at: 1461113959088,
+  // },
 ];
 
 const renderTweets = function (tweets) {
@@ -37,7 +37,7 @@ const renderTweets = function (tweets) {
   // takes return value and appends it to the tweets container
   for (let tweet of tweets) {
     let createTweet = createTweetElement(tweet);
-    $("#tweet-container").append(createTweet);
+    $("#tweet-container").prepend(createTweet);
   }
 };
 
@@ -70,27 +70,70 @@ const createTweetElement = function (tweet) {
 
 renderTweets(data);
 
+
 $("form").on("submit", function (e){
   e.preventDefault();
 
   const data = $('#tweet-text').val();
    if (data.trim() === "") {
-     return "Error, fix this!";
+   return alert("Error, field cannot be blank!");
    } else if (data.length > 140) {
-     return "Character limit exceeded"
+    return  alert("Character limit exceeded");
    } else {
      const tweetUrl = '/tweets/';
      $.ajax({
        method: 'POST',
        url: tweetUrl,
        data:  $(this).serialize(),
-       success(data){
-          console.log('Yes it works man');
-       }
-     });
-   }
-});
+       success: function(data){
+        console.log(data);
+    }
+     })
+       .then(loadTweets());
+        $(this).children('.submit-tweet').children('.counter').val('140');
+        $('#tweet-text').val('');
+    }
+  })
+   
 
 
 
+const loadTweets = function () {
+  $.ajax({
+    type: "GET",
+    url: `/tweets`,
+  })
+  .then((response) =>{
+    console.log('response = ', response);
+    renderTweets(response);
+  })
+  .catch((error) => {
+    console.log('error =', error);
+  })
+}
+loadTweets()
 
+
+
+ //  });
+    //  });
+//    }
+// });
+// ("submit", function (e){
+//   e.preventDefault();
+
+  // const data = $('#tweet-text').val();
+  //  if (data.trim() === "") {
+  //    return "Error, fix this!";
+  //  } else if (data.length > 140) {
+  //    return "Character limit exceeded"
+  //  } else {
+  //    const tweetUrl = '/tweets/';
+  //    $.ajax({
+  //      method: 'POST',
+  //      url: tweetUrl,
+  //      data:  $(this).serialize(),
+  //     })
+       
+  //   });
+  //  }
