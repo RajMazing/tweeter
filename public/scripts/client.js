b968607cb0dfbd9da2ac84f3aecdb0f3a85346f6
RajMazing <caseyraj92@gmail.com>
2021-12-30 05:03:05 +0000

    added syntax to stop duplication, refactored loadTweets and form button.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,7 +3,7 @@
 //  * jQuery is already loaded
 //  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
 //  */
-
+$(document).ready(() => {
 const escaping = function (str) {
   let div = document.createElement("div");
   div.appendChild(document.createTextNode(str));
@@ -11,34 +11,18 @@ const escaping = function (str) {
 };
 
 const data = [
-  // {
-  //   user: {
-  //     name: "Newton",
-  //     avatars: "https://i.imgur.com/73hZDYK.png",
-  //     handle: "@SirIsaac",
-  //   },
-  //   content: {
-  //     text: "If I have seen further it is by standing on the shoulders of giants",
-  //   },
-  //   created_at: 1461116232227,
-  // },
-  // {
-  //   user: {
-  //     name: "Descartes",
-  //     avatars: "https://i.imgur.com/nlhLi3I.png",
-  //     handle: "@rd",
-  //   },
-  //   content: {
-  //     text: "Je pense , donc je suis",
-  //   },
-  //   created_at: 1461113959088,
-  // },
+  
 ];
 
 const renderTweets = function (tweets) {
-  // loops through tweets
+  //$("#tweets-container").empty();
+  
+  //added syntax to stop duplicating tweets
+  $('#tweet-container').html('');
+// loops through tweets
   // calls createTweetElement for each tweet
   // takes return value and appends it to the tweets container
+
   for (let tweet of tweets) {
     let createTweet = createTweetElement(tweet);
     $("#tweet-container").prepend(createTweet);
@@ -74,39 +58,10 @@ const createTweetElement = function (tweet) {
 
 renderTweets(data);
 
-$("form").on("submit", function (e) {
-  e.preventDefault();
-
-  const data = $("#tweet-text").val();
-  if (data.trim() === "") {
-    $(".message").html("❌ Error, you can't input an empty tweet ❌");
-    $(".alert").slideDown();
-    //  return "Error, you can't input an empty tweet";
-  } else if (data.length > 140) {
-    $(".message").html("❌ Character limit exceeded ❌");
-    $(".alert").slideDown();
-    //  return "Character limit exceeded";
-  } else {
-    const tweetUrl = "/tweets/";
-    $(".message").html("");
-    $(".alert").slideUp();
-    $.ajax({
-      method: "POST",
-      url: tweetUrl,
-      data: $(this).serialize(),
-      success: function (data) {
-        console.log(data);
-      },
-    }).then(loadTweets());
-    $(".counter").val("140");
-    $("#tweet-text").val("");
-  }
-});
-
 const loadTweets = function () {
   $.ajax({
     type: "GET",
-    url: `/tweets`,
+    url: `http://localhost:8080/tweets`,
   })
     .then((response) => {
       console.log("response = ", response);
@@ -116,27 +71,49 @@ const loadTweets = function () {
       console.log("error =", error);
     });
 };
+
 loadTweets();
 
-//  });
-//  });
-//    }
-// });
-// ("submit", function (e){
-//   e.preventDefault();
 
-// const data = $('#tweet-text').val();
-//  if (data.trim() === "") {
-//    return "Error, fix this!";
-//  } else if (data.length > 140) {
-//    return "Character limit exceeded"
-//  } else {
-//    const tweetUrl = '/tweets/';
-//    $.ajax({
-//      method: 'POST',
-//      url: tweetUrl,
-//      data:  $(this).serialize(),
-//     })
+$("form").on("submit", function (e) {
+  e.preventDefault();
+  const data = $("#tweet-text").val();
 
-//   });
-//  }
+  if (data.trim() === "") {
+    $(".message").html("❌ Error, you can't input an empty tweet ❌");
+    $(".alert").slideDown();
+    //  return "Error, you can't input an empty tweet";
+  } else if (data.length > 140) {
+    $(".message").html("❌ Character limit exceeded ❌");
+    $(".alert").slideDown();
+    //  return "Character limit exceeded";
+  } else {
+    $(".message").html("");
+    $(".alert").slideUp();
+
+    const tweetData =  $(this).serialize();
+    const tweetUrl = "/tweets/";
+    
+    $.ajax({
+      method: "POST",
+      url: tweetUrl,
+      data: tweetData,
+    })
+
+   .then((response) => {
+     loadTweets();
+    
+    // // $("#tweet-text").val("");
+  })
+  .catch((error) => {
+});
+
+$("#tweet-text").val('');
+
+ $("#counter").val("140");
+  }
+});
+
+});
+
+
